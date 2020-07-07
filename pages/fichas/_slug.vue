e8a82bff71792a3c54ca4a02a4e4c1e05e127ebb
Omar Florez <omarmfs98@protonmail.com>
2020-07-06 20:29:32 -0500

    center image

--- pages/fichas/_slug.vue
+++ pages/fichas/_slug.vue
@@ -8,7 +8,7 @@
 		</div>
 		<h1 class="text-2xl lg:text-3xl font-bold text-center mb-6">Ficha Técnica</h1>
 		<div class="max-w-sm w-full lg:max-w-full lg:flex rounded">
-			<img class="h-64 img-article lg:h-auto lg:w-64 flex-none bg-cover rounded text-center overflow-hidden" :src="article.img" :title="article.alt" />
+			<img class="h-64 img-article lg:h-auto lg:w-64 flex-none bg-cover rounded text-center overflow-hidden mx-auto" :src="article.img" :title="article.alt" />
 			<div class="w-full rounded-b lg:rounded-b-none lg:rounded-r lg:px-6 py-4 flex flex-col leading-normal">
 				<div class="mb-8">
 					<div class="text-center text-gray-900 font-bold text-xl mb-2 lg:text-left">{{ article.title }}</div>
