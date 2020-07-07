77a16cbf8d8476e3ff20ae25047381fc83bf6107
Omar Florez <omarmfs98@protonmail.com>
2020-07-06 20:26:44 -0500

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,14 +17,5 @@ module.exports = {
       })
     })
   ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+  purge: false
 }
